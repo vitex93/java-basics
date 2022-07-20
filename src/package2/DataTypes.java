7dc74f2743eb29542e1dcc133b3035a27a048fe8
vitex <vitex1993@gmail.com>
2022-07-20 15:59:06 +0200

    data types examples

---
+++ src/package2/DataTypes.java
@@ -0,0 +1,31 @@
+package package2;
+
+public class DataTypes {
+	
+
+	public static void main(String[] args) {
+		//Integer example
+		int a = 5;
+		int b = 7;
+		System.out.println(a+b);
+		
+		//Double example
+		double c = 2.5;
+		double d = 2;
+		System.out.println(c*d);
+		
+		//String example
+		String name = "Darko";
+		System.out.println(name);
+		
+		//Boolean example
+		Boolean isGood = false;
+		if(isGood) {
+			System.out.println("Good!");
+		}else {
+			System.out.println("Bad");
+		}
+		
+	}
+
+}
